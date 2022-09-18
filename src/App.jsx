7548772a75612fea15e74c2fd936eef59d02a3e4
Anthony Romaine <anthony.romaine1@gmail.com>
2022-09-18 00:16:18 +0000

    Add screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { useSelector } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
+import Income from './components/Income/Income';
+import Overview from './components/Overview/Overview';
+import Expenses from './components/Expenses/Expenses';
 import './App.css'
+
+function getScreen(view) {
+  if (view === 'expenses') {
+    return <Expenses />
+  } else if (view === 'income') {
+    return <Income />
+  } else {
+    return <Overview />
+  }
+}
 
 function App() {
   const { view } = useSelector((state) => state.transactions);
@@ -9,6 +22,7 @@ function App() {
     <div className="App">
       <h1>Transactions</h1>
       <Navbar />
+      {getScreen(view)}
     </div>
   )
 }
---
+++ src/components/Overview/Overview.jsx
@@ -0,0 +1,7 @@
+export default function Overview() {
+  return (
+    <section>
+      <h2>Overview</h2>
+    </section>
+  )
+}
